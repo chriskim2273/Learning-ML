0846c4bae4ea87fa90104cd4b0a57971106f9151
chriskim2273 <christopher2273@gmail.com>
2023-02-17 22:54:48 -0500

    Added imports

--- basic_nn.py
+++ basic_nn.py
@@ -1,1 +1,10 @@
 # Hello Machine Learning...!
+
+# imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
